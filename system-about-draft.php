ddb5f45ca0784196c49ac104f5116884d6990c00
ian <ianerdestabillo@gmail.com>
2017-09-19 19:12:32 +0800

    initial version

---
+++ system-about-draft.php
@@ -0,0 +1,21 @@
+<!--Modal for admin login-->
+	 <div class="modal fade" id="login">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title"><span style="color:orange;font-family:'typo' ">ABOUT THE SYSTEM</span></h4>
+      </div>
+      <div class="modal-body title1">
+		<div class="system-about">
+
+
+
+		</div>
+      </div>
+      <!--<div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>-->
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
